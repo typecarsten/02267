aa4daeecb9cefb5a80c67206c9029c33fffae504
Carsten Jacobsen <callefix91@gmail.com>
2013-11-07 15:56:27 +0100

    Allmost done with getHotel

--- HotelReservation/src/java/Hotel/Hotel.java
+++ HotelReservation/src/java/Hotel/Hotel.java
@@ -15,15 +15,45 @@ public class Hotel {
     private Boolean creditCardGuarantee = null;
     private int price = 0;
     private String reservationService = null;
+    private int bookingNo = 0;
     
     
     
-    public Hotel(String name, String address, String city, Boolean creditCardGuarantee, int price, String reservationService){
+    public Hotel(String name, String address, String city, Boolean creditCardGuarantee, int price, String reservationService, int bookingNo){
         this.name = name;
         this.address = address;
         this.city = city;
         this.creditCardGuarantee = creditCardGuarantee;
         this.price = price;
         this.reservationService = reservationService;
+        this.bookingNo = bookingNo;
+    }
+    
+    public String getCity(){
+        return city;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public String getAddress(){
+        return address;
+    }
+    
+    public String getCreditCardGuarantee(){
+        return creditCardGuarantee.toString();
+    }
+    
+    public String getPrice(){
+        return Integer.toString(price);
+    }
+    
+    public String getReservationService(){
+        return reservationService;
+    }
+    
+    public String getBookingNo(){
+        return Integer.toString(bookingNo);
     }
 }
--- HotelReservation/src/java/Hotel/HotelReservation.java
+++ HotelReservation/src/java/Hotel/HotelReservation.java
@@ -16,16 +16,27 @@ import javax.jws.WebService;
 @WebService(serviceName = "HotelReservation")
 public class HotelReservation {
     
-        Hotel hotel1 = new Hotel("HenryHotel", "Wallstreet", "New York", true, 200, "FleggaardReservation");
-        Hotel hotel2 = new Hotel("ConnieHotel", "fifth-avenue", "New York", false, 400, "JohnyReservation");
-        Hotel hotel3 =
-        Hotel[] gunner = {hotel1};
+        int price = 0;
+        Hotel DreamDownTown = new Hotel("Dream Downtown", "Wallstreet 25", "New York", true, 200, "FleggaardReservation", 1);
+        Hotel CasaHotel = new Hotel("Cassa Hotel", "fifth-avenue 10", "New York", false, 400, "JohnyReservation", 2);
+        Hotel ValbyHotel = new Hotel("ValbyHotel", "Valbylanggade 5", "Copenhagen", true, 100, "Hotels.com", 3);
+        Hotel WakeUpCopenhagen = new Hotel("Wakeup Copenhagen", "Carsten Nieburhs Gade 11", "Copenhagen", true, 500, "Hotels.com", 4);
+        Hotel BlueSea = new Hotel("Blue Sea", "Dragonara Road St. Julian's", "Malta", true, 300, "Momondo.dk" ,5);
+        Hotel MarinaHotel = new Hotel("Marina Hotel", "St. Georges Bay St. Julian's", "malta", false, 1000, "NyhavnTravel" ,6);
+        Hotel AmediaHotel = new Hotel("Amedia Hotel", "Kurfüerstendamm 203", "Berlin", true, 500, "Momondo.dk", 7);
+        Hotel[] hotelList = {WakeUpCopenhagen, DreamDownTown, CasaHotel, ValbyHotel, BlueSea, MarinaHotel, AmediaHotel};
     /**
      * Web service operation
      */
     @WebMethod(operationName = "getHotel")
     public String[] getHotel(@WebParam(name = "city") String city, @WebParam(name = "arrivalDate") String arrivalDate, @WebParam(name = "depDate") String depDate) {
-        
+        String[] list = new String[10];
+        for(int i = 0; i<hotelList.length; i++){
+            if (hotelList[i].getCity() == city){
+                // make the price
+                list[0] = "" + hotelList[i].getName() + hotelList[i].getAddress() + hotelList[i].getBookingNo() + price + hotelList[i].getCreditCardGuarantee() + hotelList[i].getReservationService();
+            }
+        }
         return null;
     }
 
