be4b016de4d7440ffe583b210c6026e6eaafd465
MBennike <mbennike@gmail.com>
2013-11-14 13:53:02 +0100

    Flight done, minus bank

--- AirlineReservation/src/java/airline/Flight.java
+++ AirlineReservation/src/java/airline/Flight.java
@@ -14,17 +14,37 @@ public class Flight {
     
     private String flightStart;
     private String flightDest;
-    private Date departure;
-    private Date arrival;
+    private String departure;
+    private String arrival;
     private String carrier;
    
-    public Flight(String flightStart, String flightDest, Date departure, Date arrival, String carrier){
+    public Flight(String flightStart, String flightDest, String departure, String arrival, String carrier){
         
         this.flightStart = flightStart;
         this.flightDest = flightDest;
         this.departure = departure;
         this.arrival = arrival;
         this.carrier = carrier;
+    }
+    
+    public String getFlightStart(){
+        return flightStart;
+    }
+    
+    public String getFlightDest(){
+        return flightDest;
+    }
+    
+    public String getDeparture(){
+        return departure;
+    }
+    
+    public String getArrival(){
+        return arrival;
+    }
+    
+    public String getCarrier(){
+        return carrier;
     }
 
 }
--- AirlineReservation/src/java/airline/FlightInformation.java
+++ AirlineReservation/src/java/airline/FlightInformation.java
@@ -11,16 +11,31 @@ import java.util.Date;
  */
 public class FlightInformation {
    
-    private int bookingNo;
-    private double price;
+    private String bookingNo;
+    private String price;
     private String flightService;
     private Flight flight;
     
-    public FlightInformation(int bookingNo, double price, String flightService, Flight flight) {
+    public FlightInformation(String bookingNo, String price, String flightService, Flight flight) {
     
         this.bookingNo = bookingNo;
         this.price = price;
         this.flightService = flightService;
         this.flight = flight;
+    }
+    public String getBookingNo(){
+        return bookingNo;
+    }
+    
+    public String getPrice(){
+        return price;
+    }
+    
+    public String getFlightService(){
+        return flightService;
+    }
+    
+    public Flight getFlight(){
+        return flight;
     }
 }   
